d7a3a0a976d091607a1bb10a93dba83ba0208186
hannah nordgren <hannah.nordgren@flatironschool.com>
2013-11-19 17:51:11 -0500

    not figuring this out correctly, but wasting too much time on it

---
+++ class_imposed_challenges/regex.rb
@@ -0,0 +1,4 @@
+ /(\*\.google[^plus])/ ("*.google", "google.*", "plus.google.*", "*plus.google*")
+
+# Not sure of the syntax, but you can see where I was going here http://rubular.com/r/UNpb9cs6UG
+# Also to note, this will just sort out the *.google option.
