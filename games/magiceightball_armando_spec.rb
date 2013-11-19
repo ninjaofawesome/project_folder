d8de16cb4db9d36837f2f3fbbaaabb46f8bde44c
hannah nordgren <hannah.nordgren@flatironschool.com>
2013-11-18 22:26:34 -0500

    tried testing Armando's magic eight ball

---
+++ games/magiceightball_armando_spec.rb
@@ -0,0 +1,9 @@
+require "./magiceightball_armando.rb"
+
+define "response_generator" 
+	it "It should return a random answer"
+	expect (when 4).to eq("Yes definitely")
+end
+ 
+
+ ### Pls note, I couldn't figure out what to test for in the other two methods.
